cf5eb5198ca37c11a8406224493f22fa51313500
RML-Cambo <ltomusic69@gmail.com>
2024-10-21 16:09:11 +0700

    routor

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./index.css";
-import { Nav } from "./components/Home/nav";
-export const App = () => {
-  const [count, setCount] = useState(0);
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./partials/layout/Layout";
 
-  return (
-    <>
-      <Nav />
-    </>
-  );
-};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    // element: <h1 className="text-red-200 bg-black">Hello</h1>,
+  },
+]);
+export default router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App.jsx";
+import ReactDom from "react-dom/client";
+import React from "react";
 import "./index.css";
+import router from "./App";
+import { RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDom.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
